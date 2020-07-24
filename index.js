2146d05f61f73f6977c93c7664f72cf44551e22c
Chandrakanth <chittappa.c@northeastern.edu>
2020-07-24 13:10:47 -0400

    Adding the changes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const aws = require("aws-sdk");
-
+var ddb = new aws.DynamoDB({ apiVersion: '2012-08-10' });
 aws.config.update({region: 'us-east-1'});
 var ses = new aws.SES();
 
 exports.handler = function (event, context, callback) {
-var params = {
+var emailParams = {
     Destination: {
         ToAddresses: [
             'chittappa.c@northeastern.edu'
@@ -22,13 +22,78 @@ var params = {
             Data: "Password Reset Link"
         }
     },
-    Source: "passwordreset@prod.chandrakanthchittappa.site"
+    Source: "passwordlink@prod.chandrakanthchittappa.site"
 };
 
-ses.sendEmail(params).promise().then((data) => {
-        console.log("email successfully sent");
-    })
-    .catch((err)=>{
-        console.log("error occured"+ err)
-    })
-}
+let putParams = {
+    TableName : "PasswordLink",
+    Item : {
+        id : { S:email },
+        ttl : { N : expirationTime }
+    }
+};
+
+let queryParams = {
+    TableName : "PasswordLink",
+    Key : {
+        'id' : { S:email }
+    }
+};
+
+ddb.getItem(queryParams, (err,data) => {
+
+    if(err){
+        console.log(err);
+    }else{
+
+        if(data.Item == undefined){
+
+            ddb.putItem(putParams, (err,data) => {
+
+                if(err){
+                    console.log(err)
+                }else{
+                    ses.sendEmail(emailParams).promise().then((data) => {
+                        console.log("email successfully sent");
+                    })
+                    .catch((err)=>{
+                        console.log("error occured"+ err)
+                    })
+                }
+
+            });
+        }else{
+
+            let curr = new Date().getTime();
+
+            let ttl = Number(data.Item.ttl.N);
+
+            if(curr > ttl){
+
+                ddb.putItem(putParams, (err,data) => {
+
+                    if(err){
+                        console.log(err)
+                    }else{
+                        ses.sendEmail(emailParams).promise().then((data) => {
+                            console.log("email successfully sent");
+                        })
+                        .catch((err)=>{
+                            console.log("error occured"+ err)
+                        })
+                    }
+    
+                });
+
+            }else{
+                console.log('Email already sent in the last 60 mins for user ::'+email);
+            }
+
+        }
+
+    }
+
+});
+};
+
+
