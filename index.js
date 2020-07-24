57b158e615b1c2131c70722e821256d83fa93e86
Chandrakanth <chittappa.c@northeastern.edu>
2020-07-24 18:01:27 -0400

    Adding the changes

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const uuidv4 = require('uuid/v4');
 
 exports.handler = function (event, context, callback) {
 
-let email = event.Records[0].Sns.Message;
-let id = uuidv4();
+let payload = event.Records[0].Sns.Message;
+
+let parsePayload = JSON.parse(payload);
+let email = parsePayload.Email;
+let link = parsePayload.link;
+
 let curTime = new Date().getTime();
 let ttl = 60 * 60 * 1000;
 let expTime = (curTime + ttl).toString();
@@ -22,7 +26,7 @@ var emailParams = {
         Body: {
             Text: {
                 Charset: "UTF-8",
-                Data:  `To reset your password click on http://prod.chandrakanthchittappa.site/${id}`
+                Data:  link
             }
         },
         Subject: {
